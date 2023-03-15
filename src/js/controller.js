bcec0a97e6b4a19953ab1e1f9666715ed383dfdc
Colin Huber <cbhuber@gmail.com>
2023-03-14 20:26:43 -0600

    feat: implement search results for recipes.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 // Poly-filling
 import 'core-js/stable';
@@ -10,8 +12,7 @@ import 'regenerator-runtime/runtime';
 const controlRecipes = async function () {
   try {
     // Get hash part of the url
-    // const id = window.location.hash.slice(1);
-    const id = '5ed6604591c37cdc054bcc40'; // TODO: Remove
+    const id = window.location.hash.slice(1);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -28,9 +29,27 @@ const controlRecipes = async function () {
 
 // -------------------------------------------------------------------
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Load search results
+    await model.loadSearchResults(query);
+
+    // Render results
+    resultsView.render(model.state.search.results);
+  } catch (err) {}
+};
+
+// -------------------------------------------------------------------
+
 // Subscriber asks for data in view module
 const init = function () {
   recipeView.addHandleRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 // -------------------------------------------------------------------
---
+++ src/js/views/searchView.js
@@ -0,0 +1,26 @@
+import View from './View.js';
+
+class SearchView {
+  #parentEl = document.querySelector('.search');
+
+  getQuery() {
+    const query = this.#parentEl.querySelector('.search__field').value;
+    this.#clearInput();
+    return query;
+  }
+
+  #clearInput() {
+    this.#parentEl.querySelector('.search__field').value = '';
+  }
+
+  // Publisher
+  addHandlerSearch(handler) {
+    this.#parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      handler();
+    });
+  }
+}
+
+export default new SearchView();
